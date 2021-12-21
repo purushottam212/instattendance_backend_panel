121a1bb8b23df0be83ea4a471ab7f56345c482f7
purushottam212 <purushottamvaidya212@gmail.com>
2021-12-21 19:05:08 +0530

    initial setup with mysql config

---
+++ instattendance-admin-backend/src/main/java/com/instattendance/admin/InstattendanceAdminBackendApplication.java
@@ -0,0 +1,13 @@
+package com.instattendance.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstattendanceAdminBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstattendanceAdminBackendApplication.class, args);
+	}
+
+}
---
+++ instattendance-admin-backend/src/test/java/com/instattendance/admin/InstattendanceAdminBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instattendance.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstattendanceAdminBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
